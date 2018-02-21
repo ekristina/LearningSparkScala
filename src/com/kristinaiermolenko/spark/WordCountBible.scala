52ec7d78b105d7c776107084f5f2c27f0870f860
ekristina <panacejja@gmail.com>
2018-02-21 14:46:16 -0500

    changed bible url, added stopwords, added bible text to repo just in case

--- src/com/kristinaiermolenko/spark/WordCountBible.scala
+++ src/com/kristinaiermolenko/spark/WordCountBible.scala
@@ -16,21 +16,22 @@ object WordCountBible {
 
     // Download Bible text from project Gutenberg
     // http://www.gutenberg.org/cache/epub/10/pg10.txt
-    val bible_source = scala.io.Source.fromURL("http://www.gutenberg.org/cache/epub/10/pg10.txt").mkString.toLowerCase
-
-    // skipping first lines about project gutenberg
+    val bible_source = scala.io.Source.fromURL("http://www.gutenberg.org/files/10/10.txt").mkString.toLowerCase
+    // skipping first and last lines about project gutenberg
     // starting with `The First Book of Moses:  Called Genesis` line
-
-    val list = bible_source.split("\n").filter(_ != "\r").drop(12)
-   
-    // Load up each line of the ratings data into an RDD
+    val list = bible_source.split("\n").slice(37, 99850)
     val lines = sc.parallelize(list)
 
+    // OR
+    // val lines = sc.textFile("./bible_kj.txt")
+
+    val stopwords = scala.io.Source.fromFile("./english_stop_words.txt").getLines().toList
+
     // another way of splitting would be:
-    // lines.flatMap(x => x.split("\\W+")) but then I lose
+    // lines.flatMap(x => x.split("\\W+"))
     val words = lines
-      .map({line => line.replaceAll("[^a-zA-Z ']", "")})
-      .flatMap(x => x.split(" "))
+      .map({line => line.replaceAll("[^a-zA-Z ]", "").toLowerCase()})
+      .flatMap(x => x.split(" ")).filter(!stopwords.contains(_))
 
     val wordCounts = words
       .map(x => (x, 1))
